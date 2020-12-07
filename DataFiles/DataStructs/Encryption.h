b1a62eb85d627b3aacc93912b5feb0a290b0e23b
Keegan <kgm1328@live.missouristate.edu>
2020-12-07 17:21:48 -0600

    basics of encryption class

---
+++ DataFiles/DataStructs/Encryption.h
@@ -0,0 +1,20 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+class Encryption
+{
+private:
+	vector<int> key;		//used to decrypt and encrypt
+public:
+	Encryption();
+	
+	//functions
+	string encryption(string line);
+	string decryption(string line);
+};
+#endif
