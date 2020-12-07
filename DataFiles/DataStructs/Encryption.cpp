c2041beb3ee699770537b2deb910151ec6cd9608
Keegan <kgm1328@live.missouristate.edu>
2020-12-07 17:25:09 -0600

    started function definitions

---
+++ DataFiles/DataStructs/Encryption.cpp
@@ -0,0 +1,12 @@
+#include "Encryption.h"
+using namespace std;
+
+string Encryption::encryption(string line)
+{
+	string toEncrypt = "";
+}
+
+string Encryption::decryption(string line)
+{
+	string toDecrypt = "";
+}
