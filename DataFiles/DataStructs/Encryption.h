1df697b78705c7598863f7ae2d652785a24f0ec3
Keegan <kgm1328@live.missouristate.edu>
2020-12-08 15:32:09 -0600

    finished encryption class declaration

--- DataFiles/DataStructs/Encryption.h
+++ DataFiles/DataStructs/Encryption.h
@@ -1,4 +1,5 @@
 /*
+In all honesty, I was really struggling with this part of the project, so I googled an encryption class and altered it to fit my needs
 Hashing algorithm is FNV1a, which was chosen becasue I found it when I was researching built in hashing functions for the project
 */
 #ifndef ENCRYPTION_H
@@ -16,10 +17,18 @@ private:
 	static vector<int> scrambler;		//used to decrypt and encrypt
 public:
 	Encryption();
+	static size_t position;				//sets the position in the text file
 	
 	//functions
-	FNV1a(string input);
+	long long FNV1a(string input);
 	string encryption(string line);
 	string decryption(string line);
+	string hasher(string input);
+	string hex(long long decInput);
+	int index(string input, int size);
 };
+
+size_t Encryption::position = 0;
+string Encryption::locker = "%67#a9/k*g";
+vector<int> Encryption::scrambler{ 0,3,4,1,2,1,0,3,4,2,1 };
 #endif
