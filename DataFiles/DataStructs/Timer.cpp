98480cf280aaf3f740c9003cf5155d63320b949b
Keegan <kgm1328@live.missouristate.edu>
2020-12-08 16:35:53 -0600

    finished defining timer class functions

--- DataFiles/DataStructs/Timer.cpp
+++ DataFiles/DataStructs/Timer.cpp
@@ -17,52 +17,71 @@ Time::Time(int YEAR, int MONTH, int DAY)
 
 Time::Time(string DAY)
 {
-	//convert day to an int, then assign to private members with calculations
+	int date = stoi(DAY);
+	year = date / 10000;
+	month = (date - year * 10000) / 100;
+	day = date % 100;
 }
 
 //Time setters and getters
 void Time::setYear(int YEAR)
 {
-
+	year = YEAR;
 }
 
 int Time::getYear()
 {
-
+	return year;
 }
 
 void Time::setMonth(int MONTH)
 {
-
+	month = MONTH;
 }
 
 int Time::getMonth()
 {
-
+	return month;
 }
 
 void Time::setDay(int DAY)
 {
-
+	day = DAY;
 }
 
 int Time::getDay()
 {
-
+	return day;
 }
 
 //Time functions
 string Time::toString(int date)
 {
-
+	string timeString;
+	stringstream tString;
+	tString << date;
+	tString >> timeString;
+	return timeString;
 }
 
 int Time::toInt(string date)
 {
-
+	int timeInt = 0;
+	stringstream tInt;
+	tInt << date;
+	tInt >> timeInt;
+	return timeInt;
 }
 
 string Time::timeNow()
 {
-
+	string currentTime;
+	SYSTEMTIME systemTime;
+	GetLocalTime(&systemTime);
+	currentTime.append(toString(systemTime.wYear)).append("/");
+	currentTime.append(toString(systemTime.wMonth)).append("/");
+	currentTime.append(toString(systemTime.wDay)).append(" ");
+	currentTime.append(toString(systemTime.wHour)).append(":");
+	currentTime.append(toString(systemTime.wMinute));
+	return currentTime;
 }
