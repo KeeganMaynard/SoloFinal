b72e2040518923ee2981bf8909d94af59ee53689
Keegan <kgm1328@live.missouristate.edu>
2020-12-07 23:29:25 -0600

    class members and functions declared

---
+++ DataFiles/UserTypes/Member.h
@@ -0,0 +1,29 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+#include "Users.h"
+using namespace std;
+
+class Member : public User
+{
+private:
+	string memberAddress;
+	string memberPhone;
+public:
+	//constructors
+	Member();
+	Member(string userName, string userID, string userPass, string userType, string login, string address, string phone);
+	Member(string userName, string userID, string userPass, string userType, string address, string phone);
+
+	//setters and getters
+	void setAddress(string address);
+	string getAddress();
+	void setPhone(string phone);
+	string getPhone();
+
+	//functions
+	void displayData();
+	void getHistory(vector<string> acntHist);
+	void saveData();
+	void constructMemeber(string fileID);
+};
+#endif
