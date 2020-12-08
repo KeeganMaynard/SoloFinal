314924de4e7af8089381b7e1bb871f40028de3ea
Keegan <kgm1328@live.missouristate.edu>
2020-12-07 22:37:32 -0600

    class functions and members declared

---
+++ DataFiles/UserTypes/Timer.h
@@ -0,0 +1,37 @@
+#ifndef TIMER_H
+#define TIMER_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+class Time
+{
+private:
+	int year;
+	int month;
+	int day;
+public:
+	//constructors and destructor
+	Time();
+	Time(int YEAR, int MONTH, int DAY);
+	Time(string DAY);
+	~Time();
+
+	//setters and getters
+	void setYear(int YEAR);
+	int getYear();
+	void setMonth(int MONTH);
+	int getMonth();
+	void setDay(int DAY);
+	int getDay();
+
+	//Functions
+	string toString(int date);
+	int toInt(string date);
+	string timeNow();
+};
+#endif
