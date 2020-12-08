06659519ee71b0eb027389ed972003b07a8ad567
Keegan <kgm1328@live.missouristate.edu>
2020-12-08 15:39:36 -0600

    defined the FNV1a function

--- DataFiles/DataStructs/Encryption.cpp
+++ DataFiles/DataStructs/Encryption.cpp
@@ -1,6 +1,23 @@
 #include "Encryption.h"
 using namespace std;
 
+long long Encryption::FNV1a(string input)
+{
+	input = input + locker;
+	//values are based on the psuedocode for FNV
+	long long FNVoffset = 14695981039346656037;
+	long long FNVset = 1099511628211;
+
+	long long hashValue = FNVoffset;
+	for (int i = 0; i < input.length(); i++)
+	{
+		hashValue = hashValue ^ int(input[i]);
+		hashValue *= FNVset;
+	}
+
+	return hashValue;
+}
+
 string Encryption::encryption(string line)
 {
 	string toEncrypt = "";
