80f9a0347824515b5e535e20120c445e05b74be7
Keegan <kgm1328@live.missouristate.edu>
2020-12-09 09:35:26 -0600

    updated to include every file

---
+++ BearBank.cpp
@@ -0,0 +1,27 @@
+#include "SystemAdminOS.cpp"
+#include "BankOfficialOS.cpp"
+#include "BankMemberOS.cpp"
+//include statements for all user types
+#include "DataFiles/Users.h"
+#include "DataFiles/SystemAdmin.h"
+#include "DataFiles/Official.h"
+#include "DataFiles/Member.h"
+//include statements for accounts
+#include "DataFiles/Account/Account.h"
+//include statements for data structs
+#include "DataFiles/DataStructs/AVL.h"
+#include "DataFiles/DataStructs/Dictionary.h"
+#include "DataFiles/DataStructs/Encryption.h"
+#include "DataFiles/DataStructs/LinkedList.h"
+#include "DataFiles/DataStructs/Node.h"
+#include "DataFiles/DataStructs/Queue.h"
+#include "DataFiles/DataStructs/Timer.h"
+#include "DataFiles/Controller.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "test compile";
+	return 0;
+}
